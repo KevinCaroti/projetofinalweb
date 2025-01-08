36032642f1ff4c6770a35dfe966b43b0a8315c5b
kevin <kevincaroti2005@gmail.com>
2025-01-08 17:54:23 +0000

    Add project files.

---
+++ projetofinalweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetofinalweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
