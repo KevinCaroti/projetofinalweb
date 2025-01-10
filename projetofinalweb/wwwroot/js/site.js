d5edbe493cf7afc6f3d4b65dd7df98f8761aa153
Samuel Ventura <samuelventura12321@gmail.com>
2025-01-10 10:43:55 +0000

    .

--- projetofinalweb/wwwroot/js/site.js
+++ projetofinalweb/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const images = document.querySelectorAll('.book-image');
+
+// Adiciona o evento de clique para cada imagem
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        image.classList.toggle('zoomed'); // Alterna a classe 'zoomed'
+    });
+});
